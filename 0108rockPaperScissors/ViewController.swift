bca7a075046a09d9d9ad9c1fa9269afba46e19b1
Lesleytsai9 <e861k91677@gmail.com>
2020-01-08 17:10:26 +0800

    Initial Commit

---
+++ 0108rockPaperScissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  0108rockPaperScissors
+//
+//  Created by 蔡家雯 on 2020/1/8.
+//  Copyright © 2020 lesley tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 0108rockPaperScissorsTests/_108rockPaperScissorsTests.swift
@@ -0,0 +1,34 @@
+//
+//  _108rockPaperScissorsTests.swift
+//  0108rockPaperScissorsTests
+//
+//  Created by 蔡家雯 on 2020/1/8.
+//  Copyright © 2020 lesley tsai. All rights reserved.
+//
+
+import XCTest
+@testable import _108rockPaperScissors
+
+class _108rockPaperScissorsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
